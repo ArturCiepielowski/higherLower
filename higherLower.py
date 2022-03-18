8dca23201bfdb8a1fa29c213503025131e79d805
Artur Ciepielowski <artciepi@gmail.com>
2022-03-18 23:14:33 +0100

    Added randome number and printed dictionary positions from list

--- higherLower.py
+++ higherLower.py
@@ -1,1 +1,15 @@
-import random
+from random import randint
+from game_data import data
+from art import logo, vs
+
+randomeNumberA=randint(0,49)
+randomeNumberB= randomeNumberA - 1
+
+print(logo)
+
+print(f"Compare A: {data[randomeNumberA]['name']}, {data[randomeNumberA]['description']}, from {data[randomeNumberA]['country']} ")
+
+print(vs)
+
+print(f"Compare A: {data[randomeNumberB]['name']}, {data[randomeNumberB]['description']}, from {data[randomeNumberB]['country']} ")
+
