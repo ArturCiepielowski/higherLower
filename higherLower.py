571df6b08b820365bc62963516d59391521a390e
Artur Ciepielowski <artciepi@gmail.com>
2022-03-19 15:20:35 +0100

    Looking for bug with follower count

--- higherLower.py
+++ higherLower.py
@@ -11,32 +11,34 @@ game = True
 def compare():
     print(logo)
 
-    print(f"Compare A: {data[randomeNumberA]['name']}, {data[randomeNumberA]['description']}, from {data[randomeNumberA]['country']} ")
+    print(f"Compare A: {data[randomeNumberA]['name']}, {data[randomeNumberA]['description']}, from {data[randomeNumberA]['country']} HINT {data[randomeNumberA]['follower_count']}")
     print(f"Correct gueses {counter}")
     print(vs)
 
-    print(f"Compare A: {data[randomeNumberB]['name']}, {data[randomeNumberB]['description']}, from {data[randomeNumberB]['country']} ")
+    print(f"Compare B: {data[randomeNumberB]['name']}, {data[randomeNumberB]['description']}, from {data[randomeNumberB]['country']} HINT {data[randomeNumberB]['follower_count']}")
     print("Who has more followers?")
 
 
    
     
+while game:
+    compare()
+    answer = input("Your answer: ")
 
-compare()
-answer = input("Your answer: ")
+    if answer == "A":
+        if data[randomeNumberA]['follower_count']>data[randomeNumberB]['follower_count']:
+            print(f"{data[randomeNumberA]['name']} has more followers then {data[randomeNumberB]['name']}. You win")
+            counter = counter +1
+        elif data[randomeNumberA]['follower_count']<data[randomeNumberB]['follower_count']:
+            print(f"{data[randomeNumberA]['name']} has more followers then {data[randomeNumberB]['name']}. You loose")
+            game = False
 
-if answer == "A":
-    if data[randomeNumberA]['follower_count']>data[randomeNumberB]['follower_count']:
-       print(f"{data[randomeNumberA]['name']} has more followers then {data[randomeNumberB]['name']}. You win")
-       counter = counter +1
-    elif data[randomeNumberA]['follower_count']<data[randomeNumberB]['follower_count']:
-        print(f"{data[randomeNumberA]['name']} has more followers then {data[randomeNumberB]['name']}. You loose")
-
-elif answer == "B":
-    if data[randomeNumberB]['follower_count']>data[randomeNumberA]['follower_count']:
-       print(f"{data[randomeNumberB]['name']} has more followers then {data[randomeNumberA]['name']}. You win")
-       counter = counter +1
-    elif data[randomeNumberB]['follower_count']<data[randomeNumberA]['follower_count']:
-        print(f"{data[randomeNumberB]['name']} has more followers then {data[randomeNumberA]['name']}. You loose")
+    elif answer == "B":
+        if data[randomeNumberB]['follower_count']>data[randomeNumberA]['follower_count']:
+            print(f"{data[randomeNumberB]['name']} has more followers then {data[randomeNumberA]['name']}. You win")
+            counter = counter +1
+        elif data[randomeNumberB]['follower_count']<data[randomeNumberA]['follower_count']:
+            print(f"{data[randomeNumberB]['name']} has more followers then {data[randomeNumberA]['name']}. You loose")
+            game = False
 
    
