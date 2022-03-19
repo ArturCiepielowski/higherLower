1466ceb6c4fe043659cda0ea4d40d8acbfa863e1
Artur Ciepielowski <artciepi@gmail.com>
2022-03-19 15:06:23 +0100

    Added if logic + made comapre function

--- higherLower.py
+++ higherLower.py
@@ -4,12 +4,32 @@ from art import logo, vs
 
 randomeNumberA=randint(0,49)
 randomeNumberB= randomeNumberA - 1
+counter = 0
+game = True
 
-print(logo)
 
-print(f"Compare A: {data[randomeNumberA]['name']}, {data[randomeNumberA]['description']}, from {data[randomeNumberA]['country']} ")
+def compare():
+    print(logo)
 
-print(vs)
+    print(f"Compare A: {data[randomeNumberA]['name']}, {data[randomeNumberA]['description']}, from {data[randomeNumberA]['country']} ")
 
-print(f"Compare A: {data[randomeNumberB]['name']}, {data[randomeNumberB]['description']}, from {data[randomeNumberB]['country']} ")
+    print(vs)
 
+    print(f"Compare A: {data[randomeNumberB]['name']}, {data[randomeNumberB]['description']}, from {data[randomeNumberB]['country']} ")
+    print("Who has more followers?")
+
+    
+
+compare()
+answer = input("Your answer: ")
+
+if answer == "A":
+    if data[randomeNumberA]['follower_count']>data[randomeNumberB]['follower_count']:
+       print(f"{data[randomeNumberA]['name']} has more followers then {data[randomeNumberB]['follower_count']}. You win")
+       counter = counter +1
+    elif data[randomeNumberA]['follower_count']<data[randomeNumberB]['follower_count']:
+        print(f"{data[randomeNumberA]['name']} has more followers then {data[randomeNumberB]['follower_count']}. You loose")
+
+elif answer == "B":
+    print(f"{data[randomeNumberB]['follower_count']}")  
+   
